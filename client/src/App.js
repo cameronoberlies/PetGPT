6e6baef80164f31740450e331b63a397eb1b204b
James Pierce <james59222@gmail.com>
2023-07-14 01:17:17 -0500

    worked on App.js home adopt

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Adopt from './pages/Adopt';
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+  
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="flex-column justify-center align-center min-100-vh bg-primary">
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path='/Adopt' element={<Adopt/>}></Route>
+              
+              
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+  
+  export default App;
