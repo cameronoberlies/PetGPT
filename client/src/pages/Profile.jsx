b21cbe5c83fe1bddba1834913c535b7f40a00f19
Sophia Perez <sophiaperez@MacBook-Air.local>
2023-07-21 22:28:09 -0500

    profile card queries

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { Box, Flex, HStack, Image, Text } from "@chakra-ui/react";
 import { FaEnvelope} from "react-icons/fa";
+import {useQuery, gql} from "@apollo/client";
 
+export const QUERY_SINGLE_USER = gql`
+    query user {
+        user {
+            _id
+            username
+            favorites
+            email
+        }
+    }
+`;
+function Profile() {
+  const { loading, error, data } = useQuery(QUERY_SINGLE_USER);
+  const {email, username, favorites} = data.user;
 
-function Profile (){
-  return (
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const user = data?.user; // Use optional chaining to handle undefined 'data' or 'data.me'
+
+  if (!user) return <p>No user data found.</p>; // Handle cases where 'me' is not available yet
+
+   return (
     <Flex
       bg="#edf3f8"
       _dark={{ bg: "#3e3e3e" }}
@@ -39,8 +59,7 @@ function Profile (){
           p={8}
           display="flex"
           alignItems="left"
-        >
-          <Image
+        >  <Image
             src="https://images.unsplash.com/photo-1623930154261-37f8b293c059?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
             alt="Profile Picture"
             borderRadius="full"
@@ -60,21 +79,18 @@ function Profile (){
           borderRadius="lg"
           textAlign="left"
           mt={10}
-        >
+        >{email}
           <Text
             fontSize="4xl"
             fontWeight="bold"
             color="gray.800"
             _dark={{ color: "white" }}
           >
-           Name
+          {username}
           </Text>
           <HStack spacing={3} color="gray.800" _dark={{ color: "gray.200" }}>
           <FaEnvelope size={20} />
-          <Text fontSize="lg">Email</Text>
-          </HStack>
-          <HStack spacing={3} color="gray.700" _dark={{ color: "gray.200" }}>
-            <Text fontSize="lg">bio </Text>
+          <Text fontSize="lg"></Text>
           </HStack>
         </Box>
       </Flex>
