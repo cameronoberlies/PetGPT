b5d0edd60dc69a7b941f2329c763c5585ee5e703
Cameron Oberlies <cameron.oberlies@outlook.com>
2023-07-26 20:33:09 -0500

    add functional persisting favorites

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from './utils/theme';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FavoritesProvider } from './components/FavoritesContext';
+import theme from "./utils/theme";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { FavoritesProvider } from './components/FavoritesContext';
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <FavoritesProvider>
+createRoot(document.getElementById("root")).render(
+  // Use createRoot().render() for React 18
   <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>
-  </FavoritesProvider>
 );
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,35 +1,41 @@
-import decode from 'jwt-decode';
+// Importing jwt-decode for user auth.
+import decode from "jwt-decode";
 
 class AuthService {
   getProfile() {
-    return decode(this.getToken());
+    const token = this.getToken();
+    console.log("Decoding token:", token);
+    return token ? decode(token) : null;
   }
 
   loggedIn() {
     const token = this.getToken();
+    console.log("Checking token:", token);
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
   isTokenExpired(token) {
+    console.log("Decoding token:", token);
     const decoded = decode(token);
+    console.log("Decoded token:", decoded);
+
     if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem('id_token');
+      localStorage.removeItem("id_token");
       return true;
     }
     return false;
   }
 
   getToken() {
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
-  login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+  login(token) {
+    // Set the token in the localStorage
+    localStorage.setItem("id_token", token);
   }
-
   logout() {
-    localStorage.removeItem('id_token');
+    localStorage.removeItem("id_token");
     window.location.reload();
   }
 }
